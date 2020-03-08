5cdcac66b12ecfb58ea26b0c12a2c596519b73bb
Deepak <dipack.v@gmail.com>
2020-03-08 15:23:01 +0530

    spring

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -1,18 +1,29 @@
 package com.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
 import com.demo.service.EmployeeService;
 
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		new Starter().start();
+		ApplicationContext context = new AnnotationConfigApplicationContext(DemoAppConfig.class);
+		context.getBean(Starter.class).start();
+
 	}
 }
 
+@Component
 class Starter {
 
+	@Autowired
+	EmployeeService employeeService;
+
 	public void start() {
-		EmployeeService employeeService = new EmployeeService();
+
 		employeeService.getAllEmployees().forEach(System.out::println);
 	}
 }
--- src/main/java/com/demo/service/EmployeeService.java
+++ src/main/java/com/demo/service/EmployeeService.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.springframework.stereotype.Service;
+
 import com.demo.service.domain.Employee;
 
+@Service
 public class EmployeeService {
 
 	List<Employee> store = Stream.of(new Employee(1L, "Bob"), new Employee(2L, "Sam")).collect(Collectors.toList());
